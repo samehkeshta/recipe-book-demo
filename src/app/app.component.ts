a086dd66e9ef9c61be97dd5365517e833c159fe3
sameh <sameh.keshta@legate.com.sa>
2022-06-23 21:33:21 +0200

    Adding Navigation with Event Binding and ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  currentMenuItem = 'recipe';
+
+  onMenuItemChanged(menuItem: string) {
+    this.currentMenuItem = menuItem;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -9,9 +9,14 @@ export class HeaderComponent implements OnInit {
 
   collapsed = true;
 
+  @Output() menuItemSelected = new EventEmitter<string>();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onItemSelected(menuItem: string) {
+    this.menuItemSelected.emit(menuItem);
+  }
 }
