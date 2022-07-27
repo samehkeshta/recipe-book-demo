e125ef207f1c45abd3bd37a1254384bde03fc762
sameh <sameh.keshta@legate.com.sa>
2022-07-27 21:54:14 +0200

    Setting Up Routes and Adding Navigation to the App

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from "@angular/core";
+import { Routes } from "@angular/router";
+import { RouterModule } from '@angular/router';
+
+import { RecipesComponent } from "./recipes/recipes.component";
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+
+const appRoutes: Routes = [
+  {path: '', redirectTo: '/recipes', pathMatch: 'full'},
+  {path: 'recipes', component: RecipesComponent},
+  {path: 'shopping-list', component: ShoppingListComponent},
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule {
+}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -9,14 +9,8 @@ export class HeaderComponent implements OnInit {
 
   collapsed = true;
 
-  @Output() menuItemSelected = new EventEmitter<string>();
-
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  onItemSelected(menuItem: string) {
-    this.menuItemSelected.emit(menuItem);
   }
 }
